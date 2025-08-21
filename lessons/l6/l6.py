fe16a13097f369dee2cbdf7cf2dcfc9b69d4f2cf
Druid-0 <druid998@gmail.com>
2025-08-21 18:48:49 +0600

    l5

---
+++ lessons/l6/l6.py
@@ -0,0 +1,25 @@
+
+
+
+
+array_1 = (1,2,3,4,5,6,7)
+
+def find_item(array,target):
+
+    for i in array:
+        count = 0
+        if target == i:
+            i += count
+            print(count, ' found')
+        else:
+            i += count
+            print(count + i,"-", 'not found')
+
+find_item(array_1, 5)
+
+
+def binary_search(array, target):
+    left, right = 0, len(array) -1
+
+    while left <= right:
+        mid = (left + right) // 2
