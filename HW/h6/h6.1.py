b3cb137f96d6207769a652371e6184838f5acc08
Druid-0 <druid998@gmail.com>
2025-08-22 20:24:10 +0600

    h6

--- HW/h6/h6.1.py
+++ HW/h6/h6.1.py
@@ -1,16 +1,24 @@
 
-
-
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-
-        n = len(nums)
-        # n1 [1,2,4,5,7,33]
+        # n1 [2, 7, 11, 15]
         # n2 = target
         # n2 = target - n1
         # for i in range(n1):
         #   for j in (i - n2):
         #       if i == target:
         #           return j
-        #       else:
-        #           print("not found")
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+
+                    return [i, j]
+        # if not found
+        print("not found")
+        return []
+
+exmpl = Solution()
+print(exmpl.twoSum([2, 7, 3, 5, 11, 15], 19))
--- HW/h6/h6.py
+++ HW/h6/h6.py
@@ -4,7 +4,7 @@ class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         n = len(nums)
         for i in range(n):
-            for j in range(i + 1, n):  # Начинаем с i+1, чтобы избежать использования одного элемента дважды
+            for j in range(i + 1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-        return []  # Если решение не найдено
+        return []
